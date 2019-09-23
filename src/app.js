3292b6cc40857b897661f0d8347e862e87e65449
d_liyanage <admliyanage@gmail.com>
2019-09-23 11:44:05 +0530

    try end point test

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
 
+const PORT = 8081;
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/status',(req,res) => {
+    res.send({
+        message:'Hello World'
+    })
+});
+
+
+
+// create Server
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running at port -> ${PORT}`);
+});
